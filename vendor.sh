94f206aae0353b8cf7cc3e9ec02f64f5bf22a674
Petros Angelatos <petrosagg@gmail.com>
2016-12-22 18:01:47 -0800

    vendor: create initial infrastrure for linux vendoring
    Since our node library only uses a fraction of the kernel there is no
    point including all the files and all its history. Therefore, we clone
    it and build it while marking the files that were used during
    compilation. We delete anything that wasn't used and then we vendor the
    code.
    
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>

---
+++ vendor.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -o errexit
+
+git clone https://github.com/petrosagg/lkl-linux.git --depth=1 --branch node-lkl linux-tmp
+ 
+# Reset all access times to be in the past
+find linux-tmp | xargs touch -a -d 2015-01-01
+ 
+# Make the library. This will update the access times of only the actually needed files
+make -j $(nproc) -C linux-tmp/tools/lkl static
+
+# Delete anything that wasn't accessed
+find linux-tmp ! -path "*/.git/*" -atime +2 ! -type d ! -name .gitignore -delete
+# Delete a second time to delete empty directories
+find linux-tmp ! -path "*/.git/*" -atime +2 -type d -empty -delete
+
+# Cleanup all built objects
+git -C linux-tmp clean -ffxd
+
+# Verify that the pruned tree builds
+make -j $(nproc) -C linux-tmp/tools/lkl static
+
+# Cleanup all built objects again
+git -C linux-tmp clean -ffxd
+
+# Remove history
+rm -rf linux-tmp/.git
+
+# Vendor the code
+rm -rf src/linux
+mv linux-tmp src/linux
