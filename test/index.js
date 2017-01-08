6ae5572eed1b7193ada3ed8f13fd6d1c419bc31f
Petros Angelatos <petrosagg@gmail.com>
2017-01-08 04:24:18 -0800

    add support for fs.access()
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>

--- test/index.js
+++ test/index.js
@@ -1,17 +1,76 @@
 const lkl = require('../');
 const fs = require('fs');
+const path = require('path');
+const assert = require('assert');
+
+let mountpoint;
 
 describe('lkl', function() {
 	it('should start the kernel', function(done) {
 		lkl.startKernelSync(10 * 1024 * 1024);
 
-		let mountpoint = lkl.mountSync(__dirname + '/fixtures/test.ext4', {readOnly: false, fsType: 'ext4'});
+		mountpoint = lkl.mountSync(__dirname + '/fixtures/test.ext4', {readOnly: false, fsType: 'ext4'});
 
 		let input = fs.createReadStream(__dirname + '/index.js');
 		input.pipe(lkl.fs.createWriteStream(mountpoint + '/index.js'))
 		.on('close', function () {
-			lkl.umountSync(mountpoint);
+			// lkl.umountSync(mountpoint);
 			done();
+		});
+	});
+
+	describe('fs', function() {
+		describe('.access()', function() {
+			let removeFile = function(file) {
+				try {
+					lkl.fs.unlinkSync(file);
+				} catch (err) {
+					// Ignore error
+				}
+			};
+
+			let createFileWithPerms = function(file, mode) {
+				removeFile(file);
+				lkl.fs.writeFileSync(file, '');
+				lkl.fs.chmodSync(file, mode);
+			};
+
+			before(function() {
+				this.doesNotExist = path.join(mountpoint, '__this_should_not_exist');
+				this.readOnlyFile = path.join(mountpoint, 'read_only_file');
+				this.readWriteFile = path.join(mountpoint, 'read_write_file');
+
+				createFileWithPerms(this.readOnlyFile, 0o444);
+				createFileWithPerms(this.readWriteFile, 0o666);
+			});
+
+			after(function() {
+
+			});
+
+			it('non existent file', function(done) {
+				lkl.fs.access(this.doesNotExist, function(err) {
+					assert.notEqual(err, null, 'error should exist');
+					// assert.strictEqual(err.code, 'ENOENT');
+					// assert.strictEqual(err.path, doesNotExist);
+					done();
+				});
+			});
+
+			it('readonly file', function(done) {
+				lkl.fs.access(this.readOnlyFile, fs.F_OK | fs.R_OK, function(err) {
+					assert.strictEqual(err, null, 'error should not exist');
+					done();
+				});
+			});
+
+			it('readonly file 2', function(done) {
+				fs.access(this.readOnlyFile, fs.W_OK, (err) => {
+					assert.notEqual(err, null, 'error should exist');
+					assert.strictEqual(err.path, this.readOnlyFile);
+					done();
+				});
+			});
 		});
 	});
 });
